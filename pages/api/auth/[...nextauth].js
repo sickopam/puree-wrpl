76d6010e7dd1dff00eda3eb95a28f76eebd9f925
Almas Mirzandi <almasmirzandir@gmail.com>
2023-03-08 18:30:09 +0700

    updated schema.prisma

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,17 +7,17 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 const prisma = new PrismaClient()
 
 export const authOptions = {
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      if (user.merchantId) {
-        session.user.merchantId = user.merchantId;
-      }
-      return session;
-    },
-  },
+  // callbacks: {
+  //   session({ session, user }) {
+  //     if (session.user) {
+  //       session.user.id = user.id;
+  //     }
+  //     if (user.merchantId) {
+  //       session.user.merchantId = user.merchantId;
+  //     }
+  //     return session;
+  //   },
+  // },
   adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
